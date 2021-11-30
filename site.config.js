b590327bf09fab8419187be58a2eca5795c50d97
misty-kirie <94884538+misty-kirie@users.noreply.github.com>
2021-11-30 12:05:40 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,23 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: 'fe6acc0b88da4208afc17d2248d775e2',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'I Love Learning!',
+  domain: ilovelearning.info',
+  author: 'misty_kirie',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'べんきょうたのしいね',
+  socialImageTitle: 'I Love Learning!',
+  socialImageSubtitle: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'misty_kirie',
+  github: 'Misty-kirie',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
